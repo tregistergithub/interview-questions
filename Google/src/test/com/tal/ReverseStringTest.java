fd46c946d9b72e0d62b1898329cde85befe44d1a
tee reg <tregister@hotmail.com>
2016-12-01 15:27:32 +0200

    Formatting and JUnit assertThat changes

--- Google/src/test/com/tal/ReverseStringTest.java
+++ Google/src/test/com/tal/ReverseStringTest.java
@@ -1,18 +1,17 @@
 package com.tal;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
 public class ReverseStringTest {
-
 	@Test
 	public void testReverseStringInPlace() {
 		String s = "Shula bula, xyz3 muki";
 		char[] charArray = s.toCharArray();
 		ReverseString.reverseStrInPlaceWithRange(charArray, 12, 15);
 		System.out.println(s + " <--> " + String.valueOf(charArray));
-		assertEquals(String.valueOf(charArray), "Shula bula, 3zyx muki");
+		assertThat(String.valueOf(charArray), equalTo("Shula bula, 3zyx muki"));
 	}
-
 }
