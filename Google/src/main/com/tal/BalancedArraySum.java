ce80c73387834c0eb368127b75eafe924be9d8a2
tee reg <tregister@hotmail.com>
2016-12-06 18:12:17 +0200

    Added question - same sum in array

--- Google/src/main/com/tal/BalancedArraySum.java
+++ Google/src/main/com/tal/BalancedArraySum.java
@@ -3,12 +3,24 @@ package com.tal;
 import java.util.Arrays;
 
 public class BalancedArraySum {
+	public static final int DID_NOT_FIND_BALANCED_INDEX = -1;
 
-	public static void main(String[] args) {
-		int a[] = { 1, 1, 1, 3, 6, 1, 2, 3 };
-
-		int leftSum[] = new int[a.length];
-		int rightSum[] = new int[a.length];
+	/**
+	 * Searches the balanced element in an an array of integers.
+	 * 
+	 * Balanced element is an element which the sum of the the elements to its right 
+	 * is equal to the sum of the elements to its left. 
+	 * 
+	 * @param arr array of integers in which may contain a balanced element  
+	 * @return the index of the balanced element, or DID_NOT_FIND_BALANCED_INDEX in case such index was not found
+	 */
+	public int getBalancedIndex(int[] arr) {
+		
+		if (arr == null) {
+			throw new IllegalArgumentException( "arr must be non-null" );
+		}
+		int leftSum[] = new int[arr.length];
+		int rightSum[] = new int[arr.length];
 
 		int sumFromLeft = 0;
 		int sumFromRight = 0;
@@ -17,21 +29,23 @@ public class BalancedArraySum {
 		for (int i = 0; i < len; i++) {
 			leftSum[i] = sumFromLeft;
 			rightSum[len - i - 1] = sumFromRight;
-			int leftElement = a[i];
-			int rightElement = a[len - i - 1];
+			int leftElement = arr[i];
+			int rightElement = arr[len - i - 1];
 			sumFromLeft += leftElement;
 			sumFromRight += rightElement;
 		}
 
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(leftSum));
+		System.out.println(Arrays.toString(rightSum));
+
 		for (int i = 0; i < rightSum.length; i++) {
 			if (rightSum[i] == leftSum[i]) {
-				System.out.println("index = " + i + ", val=" + a[i]);
-				break;
+				System.out.println("index = " + i + ", val=" + arr[i] + ", sum=" + leftSum[i]);
+				return i;
 			}
 		}
 
-		System.out.println(Arrays.toString(a));
-		System.out.println(Arrays.toString(leftSum));
-		System.out.println(Arrays.toString(rightSum));
+		return DID_NOT_FIND_BALANCED_INDEX;
 	}
 }
